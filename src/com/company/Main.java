b29aaa5492df57fb14fa52456a50c9eff241a89f
RGiffari <11R02Giffari@users.noreply.github.com>
2020-10-20 23:32:34 +0700

    Intial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ *
+ * @author
+ * NAMA         : Raihan Giffari
+ * KELAS        : PBO12
+ * NIM          : 10119008
+ * Deskripsi Program    : Program ini berisi biodata pembuat
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTART" + "\n" + "__________________________");
+        System.out.println("NAMA\t\t: Raihan Giffari");
+        System.out.println("ALAMAT\t\t: Jl. Purwakarta");
+        System.out.println("TEMPAT LAHIR\t: MAJALENGKA");
+        System.out.println("TANGGAL LAHIR\t: 11 Februari 2001 (mungkin)");
+        System.out.println("__________________________");
+        System.out.println("HOBI\t\t: Melamun");
+        System.out.println("MAKES\t\t: Nasi Putih");
+        System.out.println("MIKES\t\t: Air Putih");
+    }
+}
